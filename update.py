4c594e813a04b51a9ced1571e267e9694bc1c122
WALM <lbhnik12@gmail.com>
2024-12-28 22:25:56 +0400

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 version = 0.03
 branch = 'dev'
 #Если будет версии патча отличаться, то можно скачать обнову
-patch = 1
+patch = 2
 repository = 'https://raw.githubusercontent.com/WALMFAST/walmfast/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=['main.py','script/updater.py', 'settings/version.py']
+update_packages=['languages/eng.py', 'languages/rus.py', 'main.py', 'phone_vendor/dandelion/dandelion.py', 'phone_vendor/gale/gale.py', 'phone_vendor/ice/ice.py', 'script/adb_fastboot_firmwaer.py', 'script/updater.py', 'settings/version.py']
