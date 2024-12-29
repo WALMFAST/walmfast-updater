ef5b2d20541a72b468e14ef0c66c34b04aee2238
WALM <lbhnik12@gmail.com>
2024-12-29 17:16:13 +0400

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,7 @@
 version = 1.0.0
 branch = 'main'
 #Если будет версии патча отличаться, то можно скачать обнову
-patch = 0
+patch = 1
 repository = 'https://raw.githubusercontent.com/WALMFAST/walmfast/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=[]
+update_packages=['walmfast']
