aded57e43bab5e74d2c70cbf895a514b8f57eed0
WLACDeveloper <WLAPCreatorLite@gmail.com>
2025-06-17 21:00:05 +0300

    Update update.py

--- update.py
+++ update.py
@@ -4,4 +4,4 @@ branch = 'main'
 patch = 200
 repository = 'https://raw.githubusercontent.com/WALMFAST/walmfast/refs/heads/'
 #Если версия ниже минимальной, то начнется скачивание полной версии программыы
-update_packages=['settings/version.py', 'main.py', 'main_b.py','settings/window_and_objects.py', 'settings/window_and_objects_b.py', 'start_b.bat', 'start_b.sh', 'settings/selector.py', 'settings/selector_b.py', 'images/black/ab.png', 'languages/rus.py', 'languages/eng.py', 'script/adb_fastboot_firmwaer.py', 'script/adb_fastboot_firmwaer_b.py', 'script/imageload.py', 'script/imageload_b.py']
+update_packages=['settings/version.py', 'main.py', 'main_b.py','settings/window_and_objects.py', 'settings/window_and_objects_b.py', 'start_b.bat', 'start_b.sh', 'settings/selector.py', 'settings/selector_b.py', 'image/black/ab.png', 'languages/rus.py', 'languages/eng.py', 'script/adb_fastboot_firmwaer.py', 'script/adb_fastboot_firmwaer_b.py', 'script/imageload.py', 'script/imageload_b.py']
